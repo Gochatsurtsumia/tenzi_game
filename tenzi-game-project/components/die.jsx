bdda51925c4a6278c5f6fe9a225018bd8e733e3a
Gochatsurtsumia <gochatsurtsumia05@gmail.com>
2025-05-15 00:53:14 +0400

    responsive changes

--- tenzi-game-project/components/die.jsx
+++ tenzi-game-project/components/die.jsx
@@ -4,6 +4,7 @@ export default function Die(props) {
   };
   return (
     <button
+      className={`die ${props.isHeld ? "held" : ""}`}
       onClick={props.hold}
       style={styles}
       aria-pressed={props.isHeld}
